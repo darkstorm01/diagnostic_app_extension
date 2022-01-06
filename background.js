2961e057d921e6676e03c1880af9719663db2b71
darkstorm01 <krishnamohan6364@gmail.com>
2022-01-06 20:40:30 +0530

    Fetching systemInfo

--- background.js
+++ background.js
@@ -1,6 +1,26 @@
 let color = "#3aa757";
 
-chrome.runtime.onInstalled.addListener(() => {
+/*chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log("Default background color set to %cgreen", `color: ${color}`);
-});
+});*/
+function getdetails() {
+  chrome.system.memory.getInfo((a) => {
+    console.log(a);
+  });
+
+  chrome.system.cpu.getInfo((a) => {
+    console.log(a);
+  });
+
+  chrome.system.storage.getInfo((a) => {
+    console.log(a);
+  });
+
+  chrome.system.display.getInfo((a) => {
+    console.log(a);
+  });
+}
+
+getdetails();
+setInterval(getdetails, 30000);
