f719540ce644f46d93755909c0047eddebc5a070
darkstorm01 <krishnamohan6364@gmail.com>
2022-01-07 17:34:55 +0530

    Messaging and fetching

--- background.js
+++ background.js
@@ -21,12 +21,55 @@
 }
 
 getdetails();
+
 setInterval(getdetails, 30000);*/
+
+async function getCPUinfo() {
+  try {
+    let cpuInfo = await chrome.system.cpu.getInfo();
+    return cpuInfo;
+  } catch (err) {
+    return "Error";
+  }
+}
+async function getStorageinfo() {
+  try {
+    let storageInfo = await chrome.system.storage.getInfo();
+    return storageInfo;
+  } catch (err) {
+    return "Error";
+  }
+}
+async function getMemoryinfo() {
+  try {
+    let memoryInfo = await chrome.system.memory.getInfo();
+    return memoryInfo;
+  } catch (err) {
+    return "Error";
+  }
+}
+
+getCPUinfo().then((data) => console.log(data));
+getStorageinfo().then((data) => console.log(data));
+getMemoryinfo().then((data) => console.log(data));
+
 chrome.runtime.onMessageExternal.addListener(function (
   request,
   sender,
   sendResponse
 ) {
-  console.log(request);
-  sendResponse("Hi");
+  switch (request) {
+    case "CPU":
+      console.log("CPU info requested");
+      getCPUinfo().then((data) => sendResponse(data));
+      break;
+    case "storage":
+      console.log("Storage info requested");
+      getStorageinfo().then((data) => sendResponse(data));
+      break;
+    case "memory":
+      console.log("Memory info requested");
+      getMemoryinfo().then((data) => sendResponse(data));
+      break;
+  }
 });
